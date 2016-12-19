e2639ae0b25c953ffada90b048b890b6aed610f6
anasrana <anas.rana@gmail.com>
2016-12-19 08:32:58 +0000

    deprecated region length check removed
    - output raw read coverage
    - added expelicit dependencies

--- R/read-coverage.R
+++ R/read-coverage.R
@@ -9,49 +9,41 @@
 #' @return Vector of the coverage in a given region
 #' @export
 #'
-read_cov <- function(f.name, chr, start, end, n.read) {
+read_cov <- function(f.name, chr, start, end, n_read) {
   # Create param from provided information
   param <- Rsamtools::ScanBamParam(which = rng_obj(chr, start, end))
 
-  raw.reads <- GenomicAlignments::readGAlignments(f.name, param = param,
+  raw_reads <- GenomicAlignments::readGAlignments(f.name, param = param,
       use.names = F) %>%
-    coverage() %>%
-    magrittr::extract2(chr) %>%
-    as.numeric() %>%
-    magrittr::extract(seq(start, end, by = 1))
+    IRanges::coverage() %>%
+    magrittr::extract2(chr)
 
-  read.norm <- raw.reads / n.read
+    return(raw_reads)
 
-  if (log2(length(read.norm)) == round(log2(length(read.norm)))) {
-        signal <- read.norm
-  } else {
-      if (length(read.norm) %% 2 == 0) {
-          ll <- length(read.norm) / 2 - 2^floor(log2(length(read.norm))) / 2 + 1
-          ul <- length(read.norm) / 2 + 2^floor(log2(length(read.norm))) / 2
-      } else {
-          ll <- round(length(read.norm) / 2) -
-                2^floor(log2(length(read.norm))) / 2 + 1
-          ul <- round(length(read.norm) / 2) +
-              2^floor(log2(length(read.norm))) / 2
-      }
-      signal <- read.norm[ll:ul]
-  }
+  # read.norm <- raw.reads / n.read
 
-  signal <- .sig_norm(signal)
+  # if (log2(length(read.norm)) == round(log2(length(read.norm)))) {
+  #       signal <- read.norm
+  # } else {
+  #     if (length(read.norm) %% 2 == 0) {
+  #         ll <- length(read.norm) / 2 - 2^floor(log2(length(read.norm))) / 2 + 1
+  #         ul <- length(read.norm) / 2 + 2^floor(log2(length(read.norm))) / 2
+  #     } else {
+  #         ll <- round(length(read.norm) / 2) -
+  #               2^floor(log2(length(read.norm))) / 2 + 1
+  #         ul <- round(length(read.norm) / 2) +
+  #             2^floor(log2(length(read.norm))) / 2
+  #     }
+  #     signal <- read.norm[ll:ul]
+  # }
 
-  return(signal)
-}
+  # signal <- .sig_norm(signal)
+  # signal <- .sig_norm(read.norm)
 
-.sig_norm <- function(signal) {
-  if (max(signal, na.rm = TRUE) != 0) {
-    signal <- (signal - min(signal, na.rm = TRUE)) /
-              (max(signal, na.rm = TRUE) - min(signal, na.rm = TRUE))
-  }
-
-  return(signal)
+  # return(signal)
 }
 
 rng_obj <- function(chr, start, end) {
-  GenomicRanges::GRanges(Rle(chr),
-    ranges = IRanges(start = as.integer(start), end = as.integer(end)))
+  GenomicRanges::GRanges(S4Vectors::Rle(chr),
+    ranges = IRanges::IRanges(start = as.integer(start), end = as.integer(end)))
 }
