9f69f7bd1aaa1835509c6792f63b08f9f428966a
anasrana <anas.rana@gmail.com>
2017-01-18 14:09:09 +0000

    initial testing for bam files

---
+++ tests/testthat/test-bam.R
@@ -0,0 +1,19 @@
+context("Reading bam files")
+
+test_that("", {
+  s1_reads <- nReadsBam("sample1.bam")
+  s2_reads <- nReadsBam("sample2.bam")
+
+  expect_equal(nrow(s1_reads), 4)
+  expect_equal(nrow(s2_reads), 3)
+  expect_equal(s1_reads$n_read, c(111, 131, 152, 113))
+  expect_equal(s2_reads$n_read, c(8, 157, 122))
+})
+
+test_that("chrCov reading coverage per chromosome", {
+  res_cov <- read.csv("sample1_chr1_cov.csv", stringsAsFactor = F)
+
+  cov <- chrCov("sample1.bam", "1", c(12762966, 47374980, 50387057, 241734214),
+    c(12785179, 47387403, 50400445, 241747105), 1)
+  expect_that(cov, equals(res_cov$x))
+})
