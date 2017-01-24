b3f3d38eacfd10a1ce95d9f504e22f755954bf76
anasrana <anas.rana@gmail.com>
2017-01-24 11:24:37 +0000

    added simple help for package itself.

---
+++ R/spector.R
@@ -0,0 +1,20 @@
+#' spector: SEquence COverage Roughness
+#'
+#' spector provides an efficient and fast quality control for genomic data.
+#'
+#' It has three main goals:
+#'
+#' \itemize{
+#' \item Identify regions in genomic data that are badly sequenced.
+#' \item Determine sample quality.
+#' \item Allow for easy comparison of quality between samples.
+#' }
+#'
+#' The main interface to spector quality control is \code{spector_qc()}
+#'
+#' To learn more about spector, start with the vignettes:
+#' \code{browseVignettes(package = "spector")}
+#'
+#' @docType package
+#' @name spector
+NULL
